72a4cc3e96130c251f81fb051a498304c3b7be18
Kaykao13 <lonewolfgirl123@gmail.com>
2016-06-14 21:00:41 -0400

    GUI classes moved to own folder. Account, Instrument, InventorySystem classes more organized and closer to completion; the code there is largely untested, however.

--- src/musicinventorysystem/Account.java
+++ src/musicinventorysystem/Account.java
@@ -1,37 +1,63 @@
 package musicinventorysystem;
 
-public class Account {
+/* TABLE OF CONTENTS (Use ctrl+f to navigate, case sensitive)
+ * 
+ * 1- VARIABLES
+ * 2- CONSTRUCTORS
+ * 3- GETTER METHODS
+ * 4- OTHER METHODS
+ */
+
+/**A class to hold basic information about the currently logged in user.
+ * @author Erica Garand*/
+public class Account implements Comparable {
+	
+	///////////////////////
+	////// VARIABLES //////
+	///////////////////////
+	
 	/**A string. The first name of the account holder*/
 	private String fname;
+	
 	/**A string. The last name of the account holder*/
 	private String lname;
+
+	/**A string. The username of the account holder.*/
+	private String uname;
+	
+	/**A string. the password of the account holder.*/
+	private String pword;
+
 	/**A boolean. True is the user has admin privileges, False if they're a regular user. Note that this wouldn't
 	be an option at registration or anything; the one admin profile to rule them all would have to be manually assigned
 	an admin through one of us editing the database.*/
 	private boolean admin;
+	
 	/**A boolean. True if the user has an instrument currently signed out. False if they do not.*/
 	private boolean isUsingInstrument;
 	
-	/**Creates a new account object using the built in openNewUser() method.
+	//////////////////////////
+	////// CONSTRUCTORS //////
+	//////////////////////////
+
+	/**Creates a new account object.
 	@param fname - the first name of the logged in user
 	@param lname - the last name of the logged in user
 	@param admin - whether or not the logged in user has admin priveliges
 	@param isUsingInstrument - whether or not the logged in user already as an instrument signed out*/
-	public Account(String fname, String lname, boolean admin, boolean isUsingInstrument){
-		openNewUser(fname, lname, admin, isUsingInstrument);
-	}
-	
-	/**Allows the account object to be reused by reassigning all the object's parameters.
-	@param fname - the first name of the logged in user
-	@param lname - the last name of the logged in user
-	@param admin - whether or not the logged in user has admin priveliges
-	@param isUsingInstrument - whether or not the logged in user already as an instrument signed out*/
-	public void openNewUser(String fname, String lname, boolean admin, boolean isUsingInstrument){
+	public Account(String fname, String lname, String uname, String pword, boolean admin, boolean isUsingInstrument){
 		this.fname = fname;
 		this.lname = lname;
+		this.uname = uname;
+		this.pword = pword;
 		this.admin = admin;
 		this.isUsingInstrument = isUsingInstrument;
 	}
+
+	
+	////////////////////////////
+	////// GETTER METHODS //////
+	////////////////////////////
 	
 	/**@return True is the user has admin privileges, False if they're a regular user.*/
 	public boolean isAdmin(){
@@ -49,10 +75,53 @@ public class Account {
 	public String getLastName(){
 		return lname;
 	}
-	
+	/**@return The username of the account holder*/
+	public String getUsername(){
+		return uname;
+	}
+	/**@return The password of the account holder*/
+	public String getPassword(){
+		return pword;
+	}
+
 	/**@return the full name and admin status of the account holder as a string.*/
 	public String toString(){
 		return fname + " " + lname + " - Admin: " + admin;
 	}
 
+	
+	///////////////////////////
+	////// OTHER METHODS //////
+	///////////////////////////
+
+	/**Compares two accounts by matching the first and last names of the account holders.
+	 * @param o - The Account object to compare to this one.*/
+	public int compareTo(Object o) {
+		if (o.getClass() != Account.class)
+			throw new IllegalArgumentException("You cannot compare any random object to an Account.");
+		Account a = (Account)o;
+		
+		return ((fname + " " + lname).compareTo(a.getFirstName() + " " + a.getLastName() == 0));
+	}
+	
+	
+	/**Compares two accounts. If both the username and the password match, the accounts match. Otherwise, just the
+	 * usernames are compared to see which Account is "less" or "greater". Default case is less than.
+	 * @param o - The Account object to compare to this one.*/
+	public int compareTo(Object o) {
+		if (o.getClass() != Account.class)
+			throw new IllegalArgumentException("You cannot compare any random object to an Account.");
+		Account a = (Account)o;
+		
+		if (this.getUsername().equals(a.getUsername()) == true && this.getPassword().equals(a.getPassword()) == true)
+			return 0;
+		else if (this.getUsername().compareTo(a.getUsername) < 0)
+			return -1;
+		else if (this.getUsername().compareTo(a.getUsername) > 0)
+			return 1;
+		else
+			return -1;
+	}
+	
+	
 }
--- src/musicinventorysystem/InventorySystem.java
+++ src/musicinventorysystem/InventorySystem.java
@@ -1,167 +1,235 @@
 package musicinventorysystem;
 
-import java.util.LinkedList;
+import java.util.List;
 import java.lang.reflect.Array;
 
-/**
- *
- * @author Matt Gulbronson
+/* TABLE OF CONTENTS (Use ctrl+f to navigate, case sensitive)
+ * 
+ * 1- VARIABLES
+ * 2- CONSTRUCTORS
+ * 3- GETTER METHODS
+ * 4- SETTER METHODS
+ * 5- OTHER METHODS
  */
+
+/**A class that holds the inventory data in memory and manipulates it as needed.
+ * @author Erica Garand*/
 public class InventorySystem {
 
-    static Account testUser;
-    static LinkedList<User> users = new LinkedList();
-    static LinkedList<Instrument> instruments = new LinkedList();
-    static LinkedList<Instrument> instrumentsOut = new LinkedList();
-    static LinkedList<Instrument> instrumentsIn = new LinkedList();
-    
-    public static void main(String[] args) {
-	testUser = new Account("TheMighty", "Admin", true, false);
+	///////////////////////
+	////// VARIABLES //////
+	///////////////////////
+
+	/**A number that points to the spot in the user list that contains the information of the currently logged in user.*/
+	private int currentUser;
 	
-	instruments = new LinkedList<Instrument>();
-	instruments.add(new Instrument("Clarinet", 1, true, "", "Bilbo Baggins,Corrin of Nohr"));
-	instruments.add(new Instrument("Trumpet", 2, true, "", ""));
-	instruments.add(new Instrument("Trombone", 3, false, "John Doe", "Bilbo Baggins,John Doe"));
-	instruments.add(new Instrument("Cow Bell", 4, false, "Out For Service", ""));
+	/**A list that holds the information of all registered users in memory.*/
+	private List<Account> users = new List();
 	
-	//Test your methods below...
+	/**A list that holds the information of all registered instruments in memory.*/
+	private List<Instrument> instruments = new List();
+
+	
+	//////////////////////////
+	////// CONSTRUCTORS //////
+	//////////////////////////
+	
+	public InventorySystem(File userFile, File instrumentFile) {
+		users = new List<Account>();
+		instruments = new List<Instrument>();
+
+		// BELOW IS DUMMY DATA, FOR TESTING ONLY. The files will have to be read from to get the real data.
+		instruments.add(new Instrument("Clarinet", 1, true, "", "Bilbo Baggins,Corrin of Nohr"));
+		instruments.add(new Instrument("Trumpet", 2, true, "", ""));
+		instruments.add(new Instrument("Trombone", 3, false, "John Doe", "Bilbo Baggins,John Doe"));
+		instruments.add(new Instrument("Cow Bell", 4, false, Instrument.OUT_FOR_SERVICE, ""));
 		
-	}
+		users.add(new Account("Bilbo", "Baggins", "ahobbit", "arkenstone", false, false));
+		users.add(new Account("Corrin", "of Nohr", "yatomaster", "anankos", false, false));
+		users.add(new Account("John", "Doe", "mysteryman", "whoami", false, true));
+		users.add(new Account("TheMighty", "AdminGuy", "rulerofall","sauron", true, false))
+		
+		instruments = bubbleSort(instruments);
+		users = bubbleSort(users);
 
-    void addUser(User u) {
-        users.add(u);
-    }
-
-    void removeUser(User u) {
-        users.remove(u);
-    }
-
-    void addInstrument(Intrument i) {
-        instruments.add(i);
-    }
-    
-    void removeInstrument(Instrument i){
-        instruments.remove(i);
-    }
-    
-    
-
-    /**
-     * Sorts an unsorted array of objects which implement the comparable
-     * interface using the bubble-sort algorithm
-     *
-     * @param unsorted the array of comparable objects to be sorted
-     */
-    public void bubbleSort(Comparable[]<User> unsorted) {
-        boolean swapped;
-        do {
-            swapped = false;
-            for (int i = 0; i < unsorted.length - 1; i++) {
-                if (unsorted[i].compareTo(unsorted[i + 1].lName) > 0) {
-                    unsorted = (Comparable[]) swap(i, i + 1, unsorted);
-                    swapped = true;
-                }
-            }
-        } while (swapped);
-    }
-
-    /**
-     * Swaps two objects in an array
-     *
-     * @param m
-     * @param n
-     * @param array the array of object
-     * @return the array with the objects at indices m & n switched
-     */
-    public Object[] swap(int m, int n, Object[] array) {
-        Object temp = array[m];
-        Array.set(array, m, array[n]);
-        Array.set(array, n, temp);
-        return array;
-    }
-    
-    
-    ///////////////////////////////////////////////////////
-    //// THIS IS A SHITTON OF METHODS WE NEED TO WRITE ////
-    ///////////////////////////////////////////////////////
-    /*/
-    // Pick your methods! Write yourself down as the author in the javadoc above each
-    // method you want, and then start coding. The only things you should be using to
-    // create these methods are the static variables above; the instrument list and the
-    // user object.
-    // 
-    // These methods will be used by various actionlisteners in the GUI stuff, and
-    // have access to the files/wherever we're storing stuff, and that's what will make
-    // the program work. Don't worry about where we're getting the data or the GUI yet
-    // though, just use the dummy data I've put in the main method and the console to
-    // test if your code works.
-    //
-    // Please note that the methods don't have their parameters or returns set properly.
-    // Be sure to modify them so they take in whatever variables you need them to. And
-    // javadoc them properly!!
-    /*/
-    
-    
-    /**Allows an admin profile to manually add a user to the file.
-	 * @author ??*/
-	public static void addUser(){
-		if (testUser != null && testUser.isAdmin() == true){
-			
-		}
 	}
 	
-	/**Allows an admin profile to manually remove a user from the file.
-	 * @author ??*/
-	public static void removeUser(){
-		if (testUser != null && testUser.isAdmin() == true){
-			
-		}
+	
+	////////////////////////////
+	////// GETTER METHODS //////
+	////////////////////////////
+	
+	/**Used to retrieve a list of all the instruments a specific student has used in the past.
+	 * @author Erica Garand*/
+	public List<Instrument> getStudentSignoutHistory(){
+		return null;
+	}
+
+	/**Used to retrieve a list of all the users currently used.
+	 * @author Erica Garand*/
+	public List<Account> getUserList(){
+		return users;
+	}
+
+	/**Used to retrieve a list of all the users currently used.
+	 * @author Erica Garand*/
+	public Account getLoggedInUser(){
+		if( currentUser >= 0 && currentUser <users.size() )
+			return currentUser;
+		return null;
 	}
 	
+	
+	////////////////////////////
+	////// SETTER METHODS //////
+	////////////////////////////
+	
+	/**Allows a new user to be added to the file.
+	 * @author Erica Garand
+     * @param u - The Account object to be added to the list. Should be newly created, as
+     * Account objects are largely unchangeable once created. */
+	public void addUser(Account u){
+		//stuff
+	}
+
+	/**Allows an admin profile to manually remove a user from the user file, if necessary.
+	 * @author Erica Garand*/
+	public void removeUser(Account u){
+		if( currentUser >= 0 && currentUser < users.size() && users.get(currentUser).isAdmin() == true ){
+			//stuff
+		}
+	}
+
 	/**Allows an admin profile to manually add an instrument to the file.
-	 * @author ??*/
-	public static void addInstrumentToFile(){
-		if (testUser != null && testUser.isAdmin() == true){
-			
+	 * @author Erica Garand*/
+	public void addInstrument(Instrument a){
+		if( currentUser >= 0 && currentUser < users.size() && users.get(currentUser).isAdmin() == true ){
+			instruments.add(a);
 		}
 	}
-	
+
 	/**Allows an admin profile to manually remove an Instrument from the file.
-	 * @author ??*/
-	public static void removeInstrumentFromFile(){
-		if (testUser != null && testUser.isAdmin() == true){
-			
+	 * @author Erica Garand*/
+	public void removeInstrument(String barcode){
+		if( currentUser >= 0 && currentUser < users.size() && users.get(currentUser).isAdmin() == true ){
+			//stuff
 		}
 	}
-	
+
 	/**Allows an admin profile to indicate whether or not an instrument is out for service.
-	 * @author ??*/
-	public static void serviceAnInstrument(){
-		if (testUser != null && testUser.isAdmin() == true){
-			
+	 * @author Erica Garand*/
+	public void serviceAnInstrument(String barcode){
+		if( currentUser >= 0 && currentUser < users.size() && users.get(currentUser).isAdmin() == true ){
+			//stuff
 		}
 	}
 
 	/**Allows any profile to sign out an instrument.
-	 * @author ??*/
-	public static void signOutInstrument(){
-		if (testUser != null){
-			
+	 * @author Erica Garand*/
+	public void signOutInstrument(String barcode){
+		if( currentUser >= 0 && currentUser < users.size() ){
+			//stuff
 		}
 	}
-	
+
 	/**Allows any profile to sign back in an instrument.
-	 * @author ??*/
-	public static void signInInstrument(){
-		if (testUser != null){
-			
+	 * @author Erica Garand*/
+	public void signInInstrument(String barcode){
+		if( currentUser >= 0 && currentUser < users.size() ){
+			//stuff
 		}
 	}
 	
-	/**Used to retrieve a list of all the instruments a specific student has used in the past.
-	 * @author ??*/
-	public static LinkedList<Instrument> getStudentSignoutHistory(){
-		return null;
+	/**Used to log in a user to the program.
+	 * @author Erica Garand
+	 * @param user - The username of the user trying to log in.
+	 * @param pass - The password of the user trying to log in.*/
+	public void logIn(String user, String pass){
+		Account temp = new Account("","",user,pass,false,false);
+		
+		//search through all the accounts
+		int index = binarySearch(temp, users);
+		
+		//if a matching account it found, set currentUser to point to that account
+		if (index >= 0)
+			currentUser = index;
+		//if no matching account was found, throw an exception
+		else
+			System.out.println("Put exception throwing stuff here");
+	}
+
+	/**Used to log out the user currently using the program.
+	 * @author Erica Garand*/
+	public void logOut(){
+		//set currentUser to -1, so it points to no account
+		currentUser = -1;
+	}
+	
+	
+	///////////////////////////
+	////// OTHER METHODS //////
+	///////////////////////////
+
+	/**
+	 * Sorts an unsorted array of objects which implement the comparable
+	 * interface using the bubble-sort algorithm
+	 *
+	 * @param unsorted - the array of comparable objects to be sorted
+	 * @author Matthew Gulbronson
+	 */
+	public List<Comparable> bubbleSort(List<Comparable> unsorted) {
+		boolean swapped;
+		do {
+			swapped = false;
+			for (int i = 0; i < unsorted.size() - 1; i++) {
+				if (unsorted.get(i).compareTo(unsorted.get(i+1)) > 0) {
+					unsorted = swap(i, i + 1, unsorted);
+					swapped = true;
+				}
+			}
+		} while (swapped);
+		
+		return unsorted;
+	}
+
+	/**
+	 * Swaps two objects in an array
+	 *
+	 * @param m - the first element to be swapped.
+	 * @param n - the second element to be swapped.
+	 * @param array - the array of objects.
+	 * @return the array, with the objects at indices m & n switched
+	 * @author Matthew Gulbronson
+	 */
+	public List<Comparable> swap(int m, int n, List<Comparable> unsorted) {
+		Object temp = unsorted.get(m);
+		Array.set(unsorted, m, unsorted.get(n));
+		Array.set(unsorted, n, temp);
+		return unsorted;
+	}
+	
+	/**Does a binary search for an item in an array of items, then returns the
+	 * index of the item if it exists in the array. If the item does not exist
+	 * in the array, it returns -1. Won't work with arrays of primitives.
+	 * @author Erica Garand
+	 * @param target - The item to find. Should be of the same type as the
+	 * elements in the list.
+	 * @param list - The array to search within. Should be of the same type
+	 * as the target.*/
+	public static int binarySearch (Comparable target, List<Comparable> list){
+		int low = 0, high = list.size(), mid = 0;
+
+		while (low <= high){
+			mid = (low + high) / 2;
+			if (list.get(mid).compareTo(target) == 0)
+				return mid;
+			else if (list.get(mid).compareTo(target) > 0)
+				high = mid - 1;
+			else
+				low = mid + 1;
+		}
+
+		return -1;
 	}
 
 }
